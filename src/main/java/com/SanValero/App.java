ddca135f600d5f4b72787e5c59961f6b7e1e6b08
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2023-04-08 12:04:19 +0200

    Initial commit

---
+++ src/main/java/com/SanValero/App.java
@@ -0,0 +1,13 @@
+package SanValero.Richard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
